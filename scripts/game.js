24fab3891ee0974636910e883ddfbfb01a5446c3
Wasim Alrawas <wassimrawwas@gmail.com>
2023-11-14 17:20:50 +0000

    add more function and test

--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,36 @@
 let game = {
-    score : 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
-}
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
-function newGame(){
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
     game.score = 0;
-    game.currentGame = []
-    game.playerMoves =  []
     showScore();
-}
-function showScore(){
-    document.getElementById('score').innerText = game.score;
+    addTurn();
 }
 
-module.exports = {game, newGame ,showScore};
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
 
 
 
